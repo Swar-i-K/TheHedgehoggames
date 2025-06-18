2b119a0a586b5e6f28246855b58468240c36944e
SwarnaiKhan <79093080+SwarnaiKhan@users.noreply.github.com>
2025-06-18 08:11:25 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 400;
+canvas.height = 300;
+ctx.fillStyle = 'green';
+ctx.fillRect(50, 50, 100, 100);
