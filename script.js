531f086f53b701e5faff402663723d5961993419
SwarnaiKhan <79093080+SwarnaiKhan@users.noreply.github.com>
2025-06-18 10:07:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ canvas.width = 400;
 canvas.height = 300;
 ctx.fillStyle = 'green';
 ctx.fillRect(50, 50, 100, 100);
-assets/model/Spike Mine.glb
+<script src="https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.min.js"></script>
+Spike Mine.glb
