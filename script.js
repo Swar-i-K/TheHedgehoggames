8158efa3cdb3dfa955b1ed26294bd8db5de45686
SwarnaiKhan <79093080+SwarnaiKhan@users.noreply.github.com>
2025-06-19 16:10:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ canvas.width = 400;
 canvas.height = 300;
 ctx.fillStyle = 'green';
 ctx.fillRect(50, 50, 100, 100);
-<script src="https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.min.js"></script>
 Spike Mine.glb
+Bowling.jpg
+<script src="https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.min.js"></script>
